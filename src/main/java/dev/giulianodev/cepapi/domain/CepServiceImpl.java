1c0e82505505c29679e41e60ce0c7e40aeebfe5c
Giuliano mario <132069668+Giulianom95@users.noreply.github.com>
2026-02-28 11:32:57 -0300

    feat: valida e normaliza CEP antes de consultar provedor.

---
+++ src/main/java/dev/giulianodev/cepapi/domain/CepServiceImpl.java
@@ -0,0 +1,40 @@
+package dev.giulianodev.cepapi.domain;
+
+import dev.giulianodev.cepapi.exception.CepInvalidoException;
+import dev.giulianodev.cepapi.integration.brasilapi.client.BrasilApiClient;
+import dev.giulianodev.cepapi.integration.brasilapi.dto.BrasilApiCepResponseDTO;
+import dev.giulianodev.cepapi.api.dto.CepResponseDTO;
+import dev.giulianodev.cepapi.mapper.Brasilapimapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CepServiceImpl implements CepService {
+    private final BrasilApiClient brasilApiClient;
+
+    public CepServiceImpl(BrasilApiClient brasilApiClient) {
+        this.brasilApiClient = brasilApiClient;
+    }
+
+    @Override
+    public CepResponseDTO buscarCep(String cep) {
+        String cepNormalizado = normalizarCep(cep);
+        validarCep(cepNormalizado);
+
+        var externo = brasilApiClient.consultarCep(cepNormalizado);
+        return Brasilapimapper.toInternal(externo);
+    }
+
+    private String normalizarCep(String cep) {
+        if (cep == null) return "";
+        return cep.replace("-", "").trim();
+    }
+
+    private void validarCep(String cep) {
+        if (!cep.matches("\\d{8}")) {
+            throw new CepInvalidoException(
+                    "CEP inválido. Use 8 dígitos (ex: 01001000) ou com hífen (ex: 01001-000)."
+            );
+        }
+    }
+
+}
